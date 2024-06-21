b80a94e982a2200f2c944530cf42a069b9836cd0
AhmedArbaz <ah2797764@gmail.com>
2024-06-21 18:33:57 +0500

    setting the badge css

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,9 +6,17 @@ export class NewsItem extends Component {
     return (
       <div className="container my-2">
         <div className="card">
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:"90%",zIndex:1}}>
+       <div style={{
+        display:"flex",
+        justifyContent:"flex-end",
+        position:"absolute",
+        right:'0'
+       }}>
+
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" >
                 {source}
               </span>
+       </div>
           <img
             src={
               !imageUrl
